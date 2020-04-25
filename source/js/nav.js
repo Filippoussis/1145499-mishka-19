25accc647a60b2f4cabd627ffbef8fc88f3c272a
Filippoussis <filippoussis@iMacAndhilippos.planet-a.ru>
2020-04-25 12:24:59 +0500

    правки по js

--- source/js/nav.js
+++ source/js/nav.js
@@ -6,11 +6,11 @@
   var humburger = header.querySelector('.humburger');
 
   nav.classList.add('nav--closed');
-  humburger.classList.add('humburger');
+  humburger.classList.add('humburger--open');
 
   humburger.addEventListener('click', function () {
     nav.classList.toggle('nav--closed');
-    humburger.classList.toggle('humburger');
+    humburger.classList.toggle('humburger--open');
     humburger.classList.toggle('humburger--close');
   });
 })();
