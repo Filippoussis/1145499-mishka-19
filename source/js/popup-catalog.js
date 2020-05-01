e32700166f766300e3f2c26978f444c603bac964
Filippoussis <filippoussis@iMacAndhilippos.planet-a.ru>
2020-05-01 18:10:37 +0500

    правки по БЭМ

--- source/js/popup-catalog.js
+++ source/js/popup-catalog.js
@@ -3,7 +3,7 @@
 (function () {
   var ESC = 27;
 
-  var cart = document.querySelectorAll(".main-link--js");
+  var cart = document.querySelectorAll(".products__cart-link");
   var popup = document.querySelector(".modal--change-size");
 
   var onPopupEscPress = function (evt) {
